e64a07809882f0c9125263647b7c51701100fa7b
kyle <kyle@192.168.0.16>
2019-06-04 21:38:43 -0700

    Top 15% in comp with feature creation and outlier removal

--- kaggle/housing_price_prediction/src/explore.py
+++ kaggle/housing_price_prediction/src/explore.py
@@ -10,7 +10,7 @@ score with 10 folds.
 See README for future work.
 '''
 
-import os, argparse, util
+import os, argparse, util, math
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -20,10 +20,288 @@ from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.model_selection import KFold, cross_val_score, train_test_split
 from sklearn.ensemble import GradientBoostingRegressor
 
-def explore_data(data, dtypes):
+def replace_na_with_none(total_df):
+    # Start by replacing n/a elements with 'None' as a categoryId as per spec
+    total_df['Alley'] = total_df['Alley'].cat.add_categories(['None']).fillna('None')
+    total_df['BsmtQual'] = total_df['BsmtQual'].cat.add_categories(['None']).fillna('None')
+    total_df['BsmtCond'] = total_df['BsmtCond'].cat.add_categories(['None']).fillna('None')
+    total_df['BsmtExposure'] = total_df['BsmtExposure'].cat.add_categories(['None']).fillna('None')
+    total_df['BsmtFinType1'] = total_df['BsmtFinType1'].cat.add_categories(['None']).fillna('None')
+    total_df['BsmtFinType2'] = total_df['BsmtFinType2'].cat.add_categories(['None']).fillna('None')
+    total_df['FireplaceQu'] = total_df['FireplaceQu'].cat.add_categories(['None']).fillna('None')
+    total_df['GarageType'] = total_df['GarageType'].cat.add_categories(['None']).fillna('None')
+    total_df['GarageFinish'] = total_df['GarageFinish'].cat.add_categories(['None']).fillna('None')
+    total_df['GarageQual'] = total_df['GarageQual'].cat.add_categories(['None']).fillna('None')
+    total_df['GarageCond'] = total_df['GarageCond'].cat.add_categories(['None']).fillna('None')
+    total_df['PoolQC'] = total_df['PoolQC'].cat.add_categories(['None']).fillna('None')
+    total_df['Fence'] = total_df['Fence'].cat.add_categories(['None']).fillna('None')
+    total_df['MiscFeature'] = total_df['MiscFeature'].cat.add_categories(['None']).fillna('None')
+
+    return total_df
+
+def cluster_regression(df):
+
+    bsmt_str = 'BsmtFinSF2'
+    clf = linear_model.LinearRegression()
+    
+    #find the weights of the points given the bsmtfinsf2
+    sample_weights = [0 if (df['BsmtFinSF2'][i] == 0) else 1 for i in range(0, len(df))]
+    for i in range(0, len(df)):
+        print(f'{df[bsmt_str][i]}, {sample_weights[i]}')
+        
+    x_in = [[df[bsmt_str][i]] for i in range(0, len(df))]
+    clf.fit(X=x_in, y=df['LogSalePrice'], sample_weight=sample_weights)
+    print(f'output of fitting: {clf.coef_}')
+    
+    sns.regplot(x='BsmtFinSF2',
+                y='LogSalePrice',
+               
+                data=df)
+    plt.show()
+    
+    return
+
+def filter_zeros(col):    
+    '''
+    Takes a col in list form and passes each element through
+    the filter to return a list of weights associated with each 
+    element.
+    '''    
+    return [0 if (col[i] == 0) else 1 for i in range(0, len(col))]
+
+def filter_all_ones(col):
+    '''
+    return a list of equal weights
+    '''
+    return [1 for i in range(0, len(col))]
+
+def filter_on_value(col, value):
+    '''
+    returns a list of sample weights filtered on 
+    the value passed in.
+    '''
+    return [0 if (col[i] == value) else 1 for i in range(0,len(col))]
+
+def get_clf_cat(df, y_col, weights=None):
+    print(df)
+    train_y = df[y_col]
+    train_x = df.drop([y_col], axis=1)
+    if weights == None:
+        weights = [1 for i in range(0, len(df))]
+    clf = linear_model.LinearRegression()
+    clf.fit(X=train_x, y=train_y, sample_weight=weights)
+    return clf
+
+
+def get_clf(df, x_col, y_col, weights=None, reshape=False):
+    train_x = df[x_col]
+    if reshape:
+        train_x = train_x.values.reshape(-1,1)
+
+    train_y = df[y_col]
+    if weights == None:
+        weights = [1 for i in range(0, len(df))]
+    clf = linear_model.LinearRegression()
+    clf.fit(X=train_x, y=train_y, sample_weight=weights)
+    return clf
+
+def implement_notes(df):
+    train_y = df['LogSalePrice']
+    clfs = {} # maps col_name to classifier
+    
+    df['LogLotArea'] = df['LotArea'].apply(np.log)
+    df.drop(['LotArea'], axis=1)
+    fit_df = pd.concat([df['LogLotArea'], train_y], axis=1)
+    # now we just need to get a regression for this data
+    clfs['LogLotArea'] = get_clf(df=fit_df, x_col='LogLotArea', y_col='LogSalePrice', reshape=True)
+    
+    max_year = df['YearBuilt'].max()
+    df['YearsOld'] = df['YearBuilt'].apply(lambda x: max_year - x)
+    df['YearsOld'] = df['YearsOld'].astype('float32')
+    df.drop(['YearBuilt'], axis=1)
+    fit_df = pd.concat([df['YearsOld'], train_y], axis=1)
+    clfs['YearsOld'] = get_clf(df=fit_df, x_col='YearsOld', y_col='LogSalePrice', reshape=True)
+    
+    max_year = df['YearRemodAdd'].max()
+    df['YearsRemodOld'] = df['YearRemodAdd'].apply(lambda x: max_year - x)
+    df.drop(['YearRemodAdd'], axis=1)
+    fit_df = pd.concat([df['YearsRemodOld'], train_y], axis=1)
+    clfs['YearsRemodOld'] = get_clf(df=fit_df, x_col='YearsRemodOld', y_col='LogSalePrice', reshape=True)
+    
+
+    fit_df = pd.concat([df['MasVnrArea'], train_y], axis=1)
+    fit_df = fit_df.dropna()
+    fit_df = fit_df.reset_index(drop=True)
+    sample_weights = [0 if fit_df['MasVnrArea'][i] == 0 else 1 for i in range(0, len(fit_df))]
+    clfs['MasVnrArea'] = get_clf(df=fit_df, x_col='MasVnrArea', y_col='LogSalePrice', weights=sample_weights, reshape=True)
+    
+    fit_df = pd.concat([df['BsmtFinSF1'], train_y], axis=1)
+    fit_df = fit_df.dropna()
+    fit_df = fit_df.reset_index(drop=True)
+    sample_weights = [0 if ((fit_df['BsmtFinSF1'][i] == 0) or (fit_df['BsmtFinSF1'][i] > 2000)) else 1 for i in range(0, len(fit_df))]
+    clfs['BsmtFinSF1'] = get_clf(df=fit_df, x_col='BsmtFinSF1', y_col='LogSalePrice', weights=sample_weights, reshape=True)
+    
+    fit_df = pd.concat([df['BsmtFinSF2'], train_y], axis=1)
+    fit_df = fit_df.dropna()
+    fit_df = fit_df.reset_index(drop=True)
+    sample_weights = [0 if fit_df['BsmtFinSF2'][i] == 0 else 1 for i in range(0, len(fit_df))]
+    clfs['BsmtFinSF2'] = get_clf(df=fit_df, x_col='BsmtFinSF2', y_col='LogSalePrice', weights=sample_weights, reshape=True)
+    
+    fit_df = pd.concat([df['BsmtUnfSF'], train_y], axis=1)
+    fit_df = fit_df.dropna()
+    fit_df = fit_df.reset_index(drop=True)
+    sample_weights = [0 if fit_df['BsmtUnfSF'][i] == 0 else 1 for i in range(0, len(fit_df))]
+    clfs['BsmtUnfSF'] = get_clf(df=fit_df, x_col='BsmtUnfSF', y_col='LogSalePrice', weights=sample_weights, reshape=True)
+    
+    fit_df = pd.concat([df['TotalBsmtSF'], train_y], axis=1)
+    fit_df = fit_df.dropna()
+    fit_df = fit_df.reset_index(drop=True)
+    sample_weights = [0 if ((fit_df['TotalBsmtSF'][i] == 0) or (fit_df['TotalBsmtSF'][i] > 2000)) else 1 for i in range(0, len(fit_df))]
+    clfs['TotalBsmtSF'] = get_clf(df=fit_df, x_col='TotalBsmtSF', y_col='LogSalePrice', weights=sample_weights, reshape=True)
+
+    fit_df = pd.concat([df['2ndFlrSF'], train_y], axis=1)
+    fit_df = fit_df.dropna()
+    fit_df = fit_df.reset_index(drop=True)
+    sample_weights = [0 if fit_df['2ndFlrSF'][i] == 0 else 1 for i in range(0, len(fit_df))]
+    clfs['2ndFlrSF'] = get_clf(df=fit_df, x_col='2ndFlrSF', y_col='LogSalePrice', weights=sample_weights, reshape=True)
+
+    fit_df = pd.concat([df['LowQualFinSF'], train_y], axis=1)
+    fit_df = fit_df.dropna()
+    fit_df = fit_df.reset_index(drop=True)
+    sample_weights = [0 if fit_df['LowQualFinSF'][i] == 0 else 1 for i in range(0, len(fit_df))]
+    clfs['LowQualFinSF'] = get_clf(df=fit_df, x_col='LowQualFinSF', y_col='LogSalePrice', weights=sample_weights, reshape=True)
+
+    fit_df = pd.concat([df['GarageArea'], train_y], axis=1)
+    fit_df = fit_df.dropna()
+    fit_df = fit_df.reset_index(drop=True)
+    sample_weights = [0 if ((fit_df['GarageArea'][i] == 0) or (fit_df['GarageArea'][i] > 1200)) else 1 for i in range(0, len(fit_df))]
+    clfs['GarageArea'] = get_clf(df=fit_df, x_col='GarageArea', y_col='LogSalePrice', weights=sample_weights, reshape=True)
+    
+    fit_df = pd.concat([df['WoodDeckSF'], train_y], axis=1)
+    fit_df = fit_df.dropna()
+    fit_df = fit_df.reset_index(drop=True)
+    sample_weights = [0 if fit_df['WoodDeckSF'][i] == 0 else 1 for i in range(0, len(fit_df))]
+    clfs['WoodDeckSF'] = get_clf(df=fit_df, x_col='WoodDeckSF', y_col='LogSalePrice', weights=sample_weights, reshape=True)
+        
+    fit_df = pd.concat([df['OpenPorchSF'], train_y], axis=1)
+    fit_df = fit_df.dropna()
+    fit_df = fit_df.reset_index(drop=True)
+    sample_weights = [0 if ((fit_df['OpenPorchSF'][i] == 0) or (fit_df['OpenPorchSF'][i] > 400)) else 1 for i in range(0, len(fit_df))]
+    clfs['OpenPorchSF'] = get_clf(df=fit_df, x_col='OpenPorchSF', y_col='LogSalePrice', weights=sample_weights, reshape=True)
+
+    fit_df = pd.concat([df['EnclosedPorch'], train_y], axis=1)
+    fit_df = fit_df.dropna()
+    fit_df = fit_df.reset_index(drop=True)
+    sample_weights = [0 if ((fit_df['EnclosedPorch'][i] == 0) or (fit_df['EnclosedPorch'][i] > 400)) else 1 for i in range(0, len(fit_df))]
+    clfs['EnclosedPorch'] = get_clf(df=fit_df, x_col='EnclosedPorch', y_col='LogSalePrice', weights=sample_weights, reshape=True)
+
+    fit_df = pd.concat([df['ScreenPorch'], train_y], axis=1)
+    fit_df = fit_df.dropna()
+    fit_df = fit_df.reset_index(drop=True)
+    sample_weights = [0 if ((fit_df['ScreenPorch'][i] == 0) or (fit_df['ScreenPorch'][i] > 400)) else 1 for i in range(0, len(fit_df))]
+    clfs['ScreenPorch'] = get_clf(df=fit_df, x_col='ScreenPorch', y_col='LogSalePrice', weights=sample_weights, reshape=True)
+    
+    # easy drop everything
+    cols_to_drop = ['3SsnPorch', 'PoolArea', 'MiscVal', 'MoSold', 'YrSold', 'Street',
+                    'Alley', 'LotShape', 'Utilities', 'LandSlope', 'Condition2',
+                    'HouseStyle', 'RoofMatl', 'Exterior2nd', 'Heating', 'Electrical', 
+                    'BsmtHalfBath', 'KitchenAbvGr', 'Functional', 'MiscFeature']
+    df = df.drop(cols_to_drop, axis=1)
+    values = {'LotFrontage': 0, 'MasVnrArea': 0}
+    df = df.fillna(value=values)
+    
+    for col, reg in clfs.items():
+        print(f'{col}: {reg.coef_}')
+    
+    return df, clfs
+
+def explore_data(train_data, dtypes):
+    '''
+    Idea here is to find ways of pre-processing the 
+    data in order to create a better model. For predicting 
+    housing prices, we don't necessarily need or want to 
+    predict the outliars that well. If we predict the 
+    average well, then we should be able to predict most 
+    of the test points well. 
+    
+    We only care about the training data and the test data 
+    doesn't matter for now since we are seeing what information
+    is relevant.  Once we know what is relevant, then we do the 
+    same thing to the test data. Ideally we make one big pipeline
+    where we manipulate both the train and test data at the 
+    same time once we know what we want to do.  It could be 
+    that we want to make the manipulations into functions so that
+    we just call the function on the frame and then we copy the 
+    list of functions we call on the training data to the test data.
+    
+    '''
+    
+    columns = list(train_data)
+    # Need to retype the columns
+    for col in dtypes:
+        if col in columns:
+            # print(f'Found {col} in col list')
+            train_data[col] = train_data[col].astype(dtypes[col])
+
+    train_data_length = len(train_data)
+    train_data = replace_na_with_none(train_data) # as per the spec
+    train_data['LogSalePrice'] = train_data['SalePrice'].apply(np.log)
+    
+    train_data, regs = implement_notes(train_data) # we now have our child models
+    print(train_data.info())
+    
+    # Now that we have our child models, we want to recompute all the 
+    # rows to be these new prediction values
+    new_y_hat = {}
+    for col, reg in regs.items():
+        print(f'Working with col: {col}')
+        y_hat = reg.predict(train_data[col].values.reshape(-1,1))
+        new_y_hat[col] = y_hat
+    new_y_hat['LogSalePrice'] = train_data['LogSalePrice']
+    master_df = pd.DataFrame(new_y_hat)
+    description = master_df.describe()
+    for col in description.columns:
+        print('-'*40)
+        print(description[col])
+    
+    master_reg = linear_model.LinearRegression()
+    train_y = master_df['LogSalePrice']
+    train_x = master_df.drop('LogSalePrice', axis=1)
+    master_reg.fit(train_x, train_y) # HERE IS OUR MASTER REGRESSION
+    
+    return regs, master_reg
+    
+    # We want to try and detect outliers automatically and mark them
+    # so that we can plot and label the outliers vs the non-outliers
+    
+    # For numerical values, outlier probably means anything beyond 3 sigma
+    # For categorical data it might mean points that are beyond 3 sigma within 
+    # each of the categories of the column
+    
+    # First we attempt to detect outliers, then we plot them against the 
+    # log_sale_price to see if we are picking out good points
+    
+    for col in columns:
+        print('-'*40)
+        print(f'Working with column {col} with dtype={train_data[col].dtype}:')
+        if str(train_data[col].dtype) != 'category':
+            
+            sns.scatterplot(x=col,
+                            y='LogSalePrice',
+                            data=train_data)
+#             plt.show()
+        else:
+            sns.catplot(x=col,
+                        y='LogSalePrice',
+                        kind='swarm',
+                        data=train_data)
+            plt.show()
+#             print(f'Skipping category column ({col}) for now...')
+            
+
     sale_price = data['SalePrice']
     sale_price_str = 'SalePrice'
-    columns = list(data)
+    
     sns.set(style='darkgrid')
     
     for col in columns:
@@ -109,7 +387,7 @@ def gradient_boost(x, y):
     return best_clf
     
 def ridge_regression(x, y):
-    print('Starting Gradient Boost Regression exploration:')
+    print('Starting Ridge Regression exploration:')
     n_folds = 10
     fold_list = [i for i in range(1, 11)]
     alphas_to_test = np.arange(1,100,10)
--- kaggle/housing_price_prediction/src/main.py
+++ kaggle/housing_price_prediction/src/main.py
@@ -33,7 +33,7 @@ args = parser.parse_args()
 housing_dtypes = {
     'Id'                :'int32',
     'MSSubClass'        :'category',
-    'MSZoning'         :'category',
+    'MSZoning'          :'category',
     'LotFrontage'       :'float32',
     'LotArea'           :'int32',
     'Street'            :'category',
@@ -48,8 +48,8 @@ housing_dtypes = {
     'Condition2'        :'category',
     'BldgType'          :'category',
     'HouseStyle'        :'category',
-    'OverallQual'       :'category',
-    'OverallCond'       :'category',
+    'OverallQual'       :'int32',
+    'OverallCond'       :'int32',
     'YearBuilt'         :'int32',
     'YearRemodAdd'      :'int32',
     'RoofStyle'         :'category',
@@ -78,10 +78,10 @@ housing_dtypes = {
     '2ndFlrSF'          :'float32',
     'LowQualFinSF'      :'float32',
     'GrLivArea'         :'float32',
-    'BsmtFullBath'      :'category',
-    'BsmtHalfBath'      :'category',
-    'FullBath'          :'category',
-    'HalfBath'          :'category',
+    'BsmtFullBath'      :'float32',
+    'BsmtHalfBath'      :'float32',
+    'FullBath'          :'float32',
+    'HalfBath'          :'float32',
     'BedroomAbvGr'      :'category',
     'KitchenAbvGr'      :'category',
     'KitchenQual'       :'category',
@@ -114,13 +114,182 @@ housing_dtypes = {
     'SalePrice'         :'float32'
 }
 
+def condition_remap(x):
+    if (x == 'Artery') or (x == 'Feedr'):
+        return 'BIG_STREET'
+    elif x == 'Norm':
+        return 'NORMAL'
+    elif (x == 'RRAn') or (x == 'RRNn') or (x == 'RRNe') or (x == 'RRAe'):
+        return 'RAILROAD'
+    elif (x == 'PosN') or (x == 'PosA'):
+        return 'POSITIVE'
+    else:
+        return 'NONE'
+
+def numerical_rating(x):
+    if x == 'Po':
+        return 1
+    elif x == 'Fa':
+        return 2
+    elif x == 'TA':
+        return 3
+    elif x == 'Gd':
+        return 4
+    elif x == 'Ex':
+        return 5
+    else:
+        return 0
+    
+def functional_remap(x):
+    if x == 'Typ':
+        return 7
+    elif x == 'Min1':
+        return 6
+    elif x == 'Min2':
+        return 5
+    elif x == 'Mod':
+        return 4
+    elif x == 'Maj1':
+        return 3
+    elif x == 'Maj2':
+        return 2
+    elif x == 'Sev':
+        return 1
+    elif x == 'Sal':
+        return 0
+
+def num_to_cat(df):
+    '''
+    For making catagories out of numbers.
+    '''
+    return df
+    
+def cat_to_num(df):
+    '''
+    For making numerical values out of categories
+    '''
+    
+    df['ExterQual'] = df['ExterQual'].apply(numerical_rating)
+    df['ExterQual'] = df['ExterQual'].astype(float)
+
+    df['ExterCond'] = df['ExterCond'].apply(numerical_rating)
+    df['ExterCond'] = df['ExterCond'].astype(float)
+
+    df['BsmtQual'] = df['BsmtQual'].apply(numerical_rating)
+    df['BsmtQual'] = df['BsmtQual'].astype(float)
+
+    df['BsmtCond'] = df['BsmtCond'].apply(numerical_rating)
+    df['BsmtCond'] = df['BsmtCond'].astype(float)
+
+    df['HeatingQC'] = df['HeatingQC'].apply(numerical_rating)
+    df['HeatingQC'] = df['HeatingQC'].astype(float)
+
+    df['KitchenQual'] = df['KitchenQual'].apply(numerical_rating)
+    df['KitchenQual'] = df['KitchenQual'].astype(float)
+
+    df['FireplaceQu'] = df['FireplaceQu'].apply(numerical_rating)
+    df['FireplaceQu'] = df['FireplaceQu'].astype(float)
+    
+    df['Functional'] = df['Functional'].apply(functional_remap)
+    df['Functional'] = df['Functional'].astype(float)
+        
+    return df
+
+def fill_missing_values(df):
+    print('Number of na values for each column:')
+    for col in df.columns:
+        if col == 'LogSalePrice':
+            continue
+        
+        n_na = df[col].isna().sum()
+        if n_na > 0:
+            if str(df[col].dtype) == 'category':
+                print(f'\tColumn {col} has {n_na} na values',
+                    f'. Average value is {df[col].describe().top}')
+                df[col] = df[col].fillna(df[col].describe().top)
+                n_na = df[col].isna().sum()
+                print(f'\tAfter fillna() {col} has {n_na} na values.')
+            else:
+                print(f'\tColumn {col} has {n_na} na values',
+                  f'. Average value is {df[col].mean()}')
+                df[col] = df[col].fillna(df[col].median())
+                n_na = df[col].isna().sum()
+                print(f'\tAfter fillna() {col} has {n_na} na values.')
+    return df
+
+def add_and_drop_cols(df):
+    print(df.columns)
+    df['LogLotArea'] = df['LotArea'].apply(np.log)
+    df = df.drop(['LotArea'], axis=1)
+
+    max_year = df['YearBuilt'].max()+10
+    df['YearsOld'] = df['YearBuilt'].apply(lambda x: max_year - x)
+    df['YearsOld'] = df['YearsOld'].astype('float32')
+    df = df.drop(['YearBuilt'], axis=1)
+    
+    df['LogYearsOld'] = df['YearsOld'].apply(np.log)
+    
+    max_year = df['YearRemodAdd'].max()+10
+    df['YearsRemodOld'] = df['YearRemodAdd'].apply(lambda x: max_year - x)
+    df = df.drop(['YearRemodAdd'], axis=1)
+    df['LogYearsRemodOld'] = df['YearsRemodOld'].apply(np.log)
+    
+    df = df.assign(TotalFinishedBsmtSF=lambda df: df.BsmtFinSF1+df.BsmtFinSF2)
+    df = df.drop(['BsmtFinSF1', 'BsmtFinSF2'], axis=1)
+    
+    print(f'ex1={len(df.Exterior1st)} , ex2={len(df.Exterior2nd)}')
+    column = [0 if str(df['Exterior1st'][i]) == str(df['Exterior2nd'][i]) else 1 for i in range(0,len(df))]
+    df['HasTwoExteriors'] = column
+    
+    max_year = df['GarageYrBlt'].max()+10
+    df['YearsOldGarage'] = df['GarageYrBlt'].apply(lambda x: max_year - x)
+    df = df.drop(['GarageYrBlt'], axis=1)
+    
+    df['IsCulD'] = df['LotConfig'].apply(lambda x: 1 if x == 'CulDSac' else 0)
+    df = df.drop(['LotConfig'], axis=1)
+    
+    df['NewCond1'] = df['Condition1'].apply(condition_remap)
+    df = df.drop(['Condition1'], axis=1)
+    
+    df['BsmtBathTot'] = df.BsmtFullBath + df.BsmtHalfBath*0.5
+    df = df.drop(['BsmtFullBath', 'BsmtHalfBath'], axis=1)
+    
+    df['BathTot'] = df.FullBath + df.HalfBath*0.5
+    df = df.drop(['FullBath', 'HalfBath'], axis=1)
+
+    # easy drop everything
+    cols_to_drop = ['3SsnPorch', 'PoolArea', 'PoolQC', 'MiscVal', 'MoSold', 'YrSold', 'Street',
+                    'Alley', 'LotShape', 'Utilities', 'LandSlope', 'Condition2',
+                    'HouseStyle', 'RoofMatl', 'Exterior2nd', 'Heating', 'Electrical', 
+                    'KitchenAbvGr', 'MiscFeature']
+    df = df.drop(cols_to_drop, axis=1)
+    values = {'LotFrontage': 0, 'MasVnrArea': 0}
+    df = df.fillna(value=values)
+
+    return df
+
+def take_out_outliers(df):
+    df = df[df['LotFrontage'] < 200]
+    df = df[df['MasVnrArea'] < 1200]
+    df = df[df['TotalBsmtSF'] < 3000]
+    df = df[df['1stFlrSF'] < 3000]
+    df = df[df['GrLivArea'] < 4000]
+    df = df[df['GarageArea'] < 1200]
+    df = df[df['OpenPorchSF'] < 400]
+    df = df[df['TotalFinishedBsmtSF'] < 3000]
+    
+    df['OverallQual'] = df['OverallQual'].apply(lambda x: 3 if x <= 3 else x)
+    df['BsmtQual'] = df['BsmtQual'].apply(lambda x: 2 if x < 2 else x)
+    
+
+    return df
 
 # Try loading the data
 train = pd.read_csv(args.indir + '/train.csv')
 test = pd.read_csv(args.indir + '/test.csv')
 
 if args.explore:
-    explore.explore_data(data=train, dtypes=housing_dtypes)
+    explore.explore_data(train_data=train, dtypes=housing_dtypes)
     print('done.')
     quit()
 train_id = train['Id']
@@ -135,21 +304,25 @@ if not args.skip:
     # Drop the ids first 
     train.drop('Id', axis=1, inplace=True)
     test.drop('Id', axis=1, inplace=True)
-
+    
+    train['LogSalePrice'] = train['SalePrice'].apply(np.log)
+    train = train.drop(['SalePrice'], axis=1)
+    
+    
+    
     # 16% of rows don't have this value so drop it
-    train.drop('LotFrontage', axis=1, inplace=True)
-    test.drop('LotFrontage', axis=1, inplace=True)
+#     train.drop('LotFrontage', axis=1, inplace=True)
+#     test.drop('LotFrontage', axis=1, inplace=True)
     
     # Concatinate both sets so that we only have to use one lineto make manipulations
-    total_df = pd.concat([train, test], sort=False)
-    col_list = total_df.columns.tolist()
-    
+    total_df = pd.concat([train, test], sort=False)    
 
     # Need to retype the columns
     for col in housing_dtypes:
-        if col in col_list:
+        if col in list(total_df.columns):
             # print(f'Found {col} in col list')
             total_df[col] = total_df[col].astype(housing_dtypes[col])
+            
 
     # Start by replacing n/a elements with 'None' as a categoryId as per spec
     total_df['Alley'] = total_df['Alley'].cat.add_categories(['None']).fillna('None')
@@ -166,37 +339,24 @@ if not args.skip:
     total_df['PoolQC'] = total_df['PoolQC'].cat.add_categories(['None']).fillna('None')
     total_df['Fence'] = total_df['Fence'].cat.add_categories(['None']).fillna('None')
     total_df['MiscFeature'] = total_df['MiscFeature'].cat.add_categories(['None']).fillna('None')
-
-    # Now to check and see what the real amount of n/a responses are
-    print('-'*60)
-    print('Printing out missing elements as percentage of rows')
-    n_rows = len(total_df.index)
-    print(f'total number of rows: {n_rows}')
-    modes = total_df.mode()
-    print(modes)
-    means = total_df.mean(skipna=True, numeric_only=True)
-    print(means)
-
-    for col in col_list:
-        percent_missing = total_df[col].isna().sum() / n_rows
-
-        if housing_dtypes[col] == 'category':
-            # print(f'Filling category {col} with {modes[col][0]}')
-            total_df[col] = total_df[col].fillna(modes[col][0])
-            
-        elif (col != 'SalePrice') & (housing_dtypes[col] != 'category'):
-            # print(f'Filling numeric {col} with {means[col]}')
-            total_df[col] = total_df[col].fillna(means[col])
-
-        percent_missing_after = total_df[col].isna().sum() / n_rows
-        # print(f'{col:<15} {percent_missing:>.2%} {percent_missing_after:>.2%}')
     
+    total_df = fill_missing_values(total_df)
+    total_df = add_and_drop_cols(total_df)
+    total_df = cat_to_num(total_df)
+    total_df = num_to_cat(total_df)
     total_df = pd.get_dummies(total_df)
+    
+    
+    total_df.describe().to_csv(args.outdir + '/traindata.csv')
+    print(total_df.describe())
     
     ntrain = len(train.index)
     ntest = len(test.index)
     train = total_df[:ntrain] # from 0 to end of train
     test = total_df[ntrain:] # from ntrain to end
+    
+    train = take_out_outliers(train)
+    train.reset_index(drop=True)
 
     print(f'Train shape: {train.shape}')
     print(f'Train shape: {test.shape}')
@@ -214,48 +374,18 @@ if not args.skip:
 # Now build the model after pre-processing
 print('Building model')
 
-train_y = train['SalePrice']
-log_train_y = np.empty(len(train_y), dtype=float)
-np.log(train_y, out=log_train_y)
-train_x = train.drop(['SalePrice'], axis=1)
 
-test_y = test['SalePrice']
-test_x = test.drop(['SalePrice'], axis=1)
+train_y = train['LogSalePrice']
+train_x = train.drop(['LogSalePrice'], axis=1)
 
-# Define the models here
-clf_reg = linear_model.LinearRegression()
-clf_ridge = linear_model.Ridge(alpha=.5)
-clf_tree = tree.DecisionTreeRegressor()
-clf_grad = GradientBoostingRegressor(n_estimators=100, learning_rate=0.1,
-        max_depth=1, random_state=0, loss='ls')
-
-reg_scores = cross_val_score(clf_reg, train_x, log_train_y, cv=10)
-print(f'Linear Regression scores: {reg_scores}')
-print(f'mean: {reg_scores.mean()}, std: {reg_scores.std()}')
-
-ridge_scores = cross_val_score(clf_ridge, train_x, log_train_y, cv=10)
-print(f'Linear Ridge Regression scores: {ridge_scores}')
-print(f'mean: {ridge_scores.mean()}, std: {ridge_scores.std()}')
-
-tree_scores = cross_val_score(clf_tree, train_x, log_train_y, cv=10)
-print(f'Tree Regresion scores: {tree_scores}')
-print(f'mean: {tree_scores.mean()}, std: {tree_scores.std()}')
-
-grad_scores = cross_val_score(clf_grad, train_x, log_train_y, cv=10)
-print(f'Graidient Boost scores: {grad_scores}')
-print(f'mean: {grad_scores.mean()}, std: {grad_scores.std()}')
+test_y = test['LogSalePrice']
+test_x = test.drop(['LogSalePrice'], axis=1)
 
 
-'''
-#### FORK ####
-'''
-# best_clf = explore.gradient_boost(train_x, log_train_y)
-best_clf = explore.ridge_regression(train_x, log_train_y)
+best_clf = explore.ridge_regression(train_x, train_y)
 
+best_clf.fit(train_x, train_y)
 
-best_clf.fit(train_x, log_train_y)
-
-# Create our predictions on the test set
 predictions = best_clf.predict(test_x)
 out_pred = np.empty(len(predictions), dtype=np.float64)
 np.exp(predictions, out=out_pred)
@@ -266,7 +396,5 @@ with open(args.outdir + 'submit.csv', mode='w') as fout:
     fout.write('Id,SalePrice\n')
     for i in range(0, len(out_pred)):
         fout.write(f'{test_id.iloc[i]},{out_pred[i]}\n')
-
-
 
 print('done.')
